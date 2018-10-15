a15d8baf430ad8d594830a3d14587d1a0069e985
nikhil <nikhilagarwal32@gmail.com>
2018-10-15 09:36:30 -0700

    added master_dict and helper functions for same

--- code/descTxtStructure.py
+++ code/descTxtStructure.py
@@ -2,14 +2,14 @@
 
 class DescTxtStructure:
 
-    def __init__(self, tag_tf_df_tfidf):
-        self.tag = tag_tf_df_tfidf[0]
-        self.tf = float(tag_tf_df_tfidf[1])
-        self.df = float(tag_tf_df_tfidf[2])
-        self.tfidf = float(tag_tf_df_tfidf[3])
+    def __init__(self, term_tf_df_tfidf):
+        self.term = term_tf_df_tfidf[0]
+        self.tf = float(term_tf_df_tfidf[1])
+        self.df = float(term_tf_df_tfidf[2])
+        self.tfidf = float(term_tf_df_tfidf[3])
         
     def __str__(self):
-        return("%s %s %s %s" % (self.tag, self.tf, self.df, self.tfidf))
+        return("%s %s %s %s" % (self.term, self.tf, self.df, self.tfidf))
     
     def getVal(self, model):
         if model == "TF":
--- code/textualDescriptorProcessor.py
+++ code/textualDescriptorProcessor.py
@@ -1,25 +1,82 @@
+from constants import *
 from descTxtStructure import DescTxtStructure
+
+class TxtTermStructure:
+    
+    def __init__(self):
+        self.master_dict = dict()
+
+###
+#  Get data textual descriptors data for users from file and store it in dictionaries
+###
+
+    def loadUsersData(self):
+        self.master_dict = dict()
+        self.getDescTxtData(TEXT_DESCRIPTORS_PATH+'devset_textTermsPerUser.txt')
+        
+###
+#  Get data textual descriptors data for images from file and store it in dictionaries
+###
+
+    def loadImageData(self):
+        self.master_dict = dict()
+        self.getDescTxtData(TEXT_DESCRIPTORS_PATH+'devset_textTermsPerImage.txt')
+        
+###
+#  Get data textual descriptors datafor location from file and store it in dictionaries
+###
+
+    def loadLocationData(self):
+        self.master_dict = dict()
+        self.getDescTxtData(TEXT_DESCRIPTORS_PATH+'devset_textTermsPerPOI.wFolderNames.txt')
+        
+###
+#  Get data textual descriptors data for all from file and store it in dictionaries
+###
+
+    def loadAllTextualData(self):
+        self.master_dict = dict()
+        self.getDescTxtData(TEXT_DESCRIPTORS_PATH+'devset_textTermsPerPOI.wFolderNames.txt')
+        self.getDescTxtData(TEXT_DESCRIPTORS_PATH+'devset_textTermsPerImage.txt')
+        self.getDescTxtData(TEXT_DESCRIPTORS_PATH+'devset_textTermsPerUser.txt')
+        
 
 ###
 #  Get data textual descriptors data from file and store it in a dictionary, 
-# whose key is objectId and values is list of classes of descTxtStructure
+# whose key is objectId and values is list of objects of type descTxtStructure
 ###
 
-def getDescTxtData(filePath):
-    file_Pointer = open(filePath, 'r')
-    ### read each user's data, one line/row at a time
-    master_dict = dict()
-    for lines in file_Pointer:
-        line = lines.split()
-        ### get the user/image/location id as key
-        key_id = line[0]
-        values = []
-        ### store all this as key values pairs in a dictionary
-        if key_id in master_dict:
-            values = master_dict[key_id]
-        else:
-            master_dict[key_id] = values
-        ### store these values in a dict[user/image/location_id] ->  list of tags stored as array of objects
-        for i in range(1, len(line), 4):
-            values.append(DescTxtStructure(line[i:i+4]))
-    return master_dict
+    def getDescTxtData(self, filePath):
+        file_Pointer = open(filePath, 'r')
+        ### read each user's data, one line/row at a time
+        for lines in file_Pointer:
+            line = lines.split()
+            ### get the user/image/location id as key
+            key_id = line[0]
+            values = []
+            ### get index of first word starting with quotes, this is done to 
+            ### avoid reading name without spaces in location file
+            k = [line.index(x) for x in line[:20] if '"' in x]
+            k = k[0]
+            ### store all this as key values pairs in a dictionary
+            if key_id in self.master_dict:
+                values = self.master_dict[key_id]
+            else:
+                self.master_dict[key_id] = values
+            ### store these values in a dict[user/image/location_id] -> 
+            ### list of tags stored as array of objects
+            for i in range(k, len(line), 4):
+                values.append(DescTxtStructure(line[i:i+4]))
+
+
+###
+#  returns an array of terms for a given id (as String), if present in data else returns empty array
+#
+###
+
+    def getTerms(self, id):
+        if id not in self.master_dict:
+            print("given id: ", id, " not found")
+            return []
+        return [texDescriptor.term for texDescriptor in self.master_dict[id] ]
+        
