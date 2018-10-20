1bad098b6cf93c46f1030bbef2dfa00d5d6204b3
nikhil <nikhilagarwal32@gmail.com>
2018-10-19 23:05:16 -0700

    added progress bar and fixed glitches

--- code/task7.py
+++ code/task7.py
@@ -2,7 +2,7 @@
 This module is the program for task 7.
 '''
 from textual_descriptor_processor import TxtTermStructure
-
+import sys
 import numpy as np
 import tensorly as tl
 from tensorly.decomposition import parafac
@@ -17,42 +17,46 @@ class Task7(object):
 	'''
 	def build_tensor(self):
 		# get users term data
-	    users = TxtTermStructure()
-	    users.loadUsersData()
-	    # get images term data
-	    images = TxtTermStructure()
-	    images.loadImageData()
-	    # get locations term data
-	    locations = TxtTermStructure()
-	    locations.loadLocationData()
-	    
-	    self.tensor_arr = []
-	    print("starting tensor build...")
-	    # iterating through locations to get common terms between locations-users-images
-	    for location in locations.master_dict:
-	        location_arr = []
-	        user_arr = []
-	        # set containing location-specific terms
-	        location_set = locations.getTerms(location)
-	        # iterating through all users for the given location
-	        for user in users.master_dict:
-	            user_arr = []
-	            # build set containing terms intersecting between user and location
-	            location_user_set = location_set.intersection(users.getTerms(user))
-	            # if no intersection, skip further check for images and mark all further
-	            # number of similar tags as zero for this user
-	            if(len(location_user_set) == 0):
-	                user_arr = list(np.zeros((len(images.master_dict.keys()),), dtype=int))
-	                location_arr.append(user_arr)
-	                continue
-	            # if there are some common terms between user and location, find further
-	            # common terms between all images and this set
-	            for image in images.master_dict:
-	                user_arr.append(len(location_user_set.intersection(images.getTerms(image))))
-	            # add list of count of common terms
-	            location_arr.append(user_arr)
-	        # add list of list of the common terms to parent list
-	        self.tensor_arr.append(location_arr)
+		users = TxtTermStructure()
+		users.load_users_data()
+		# get images term data
+		images = TxtTermStructure()
+		images.load_image_data()
+		# get locations term data
+		locations = TxtTermStructure()
+		locations.load_location_data()
+		
+		self.tensor_arr = []
+		print("starting tensor build...")
+		# iterating through locations to get common terms between locations-users-images
+		for location in locations.master_dict:
+			location_arr = []
+			user_arr = []
+			# set containing location-specific terms
+			location_set = locations.get_terms(location)
+			# iterating through all users for the given location
+			for user in users.master_dict:
+				user_arr = []
+				# build set containing terms intersecting between user and location
+				location_user_set = location_set.intersection(users.get_terms(user))
+				# if no intersection, skip further check for images and mark all further
+				# number of similar tags as zero for this user
+				if(len(location_user_set) == 0):
+					user_arr = list(np.zeros((len(images.master_dict.keys()),), dtype=int))
+					location_arr.append(user_arr)
+					continue
+				# if there are some common terms between user and location, find further
+				# common terms between all images and this set
+				for image in images.master_dict:
+					user_arr.append(len(location_user_set.intersection(images.get_terms(image))))
+				# add list of count of common terms
+				location_arr.append(user_arr)
+				# print progress bar for tensor build
+				sys.stdout.write('\r')
+				sys.stdout.write("[%-30s] %d%%" % ('#'*len(self.tensor_arr), (100//len(locations.master_dict))*len(self.tensor_arr)))
+				sys.stdout.flush()
+			# add list of list of the common terms to parent list
+			self.tensor_arr.append(location_arr)
 
 	'''
 	Method: given a tensor and value of k, computes the 
@@ -71,7 +75,7 @@ class Task7(object):
 	'''
 	def runner(self):
 		# take input from user
-		k = input("Enter value of k: ")
+		k = int(input("Enter value of k: "))
 		# build tensor for location-user-images based on count of common terms
 		self.build_tensor()
 		self.compute_tensor_cp_decomposition(k)
