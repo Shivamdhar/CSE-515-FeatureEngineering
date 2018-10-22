b525a52ee13aa5dcf2f51ae1511834a418044887
ronaktannaasu <rtanna@asu.edu>
2018-10-22 14:39:21 -0700

    Single quotes to double quotes

--- code/task_6_textual_descriptors.py
+++ code/task_6_textual_descriptors.py
@@ -12,10 +12,10 @@ from util import Util
 class Task6:
 	
 	def __init__(self):
-		'''
+		"""
 		Method Explanation:
 			Intializes all the variables for the analysis task.
-		'''
+		"""
 		self.util = Util()
 		self.data_extractor = DataExtractor()
 
@@ -32,21 +32,21 @@ class Task6:
 		self.similarity_matrix = numpy.zeros((self.LOCATION_COUNT, self.LOCATION_COUNT)) # To capture location-location similarity
 
 	def construct_vocabulary(self):
-		'''
+		"""
 		Method Explanation:
 			. Constructs a global term vocabulary.
 			. Constructs a location based term vocabulary.
-		'''
-		with open(constants.TEXTUAL_DESCRIPTORS_DIR_PATH + 'devset_textTermsPerPOI.txt', encoding='utf-8') as f:
-			lines = [line.rstrip('\n') for line in f]
+		"""
+		with open(constants.TEXTUAL_DESCRIPTORS_DIR_PATH + "devset_textTermsPerPOI.txt", encoding="utf-8") as f:
+			lines = [line.rstrip("\n") for line in f]
 			for line in lines:
 				words = line.split()
 
 				temp_list_for_title = []
 				# extract location title
-				while '\"' not in words[0]:
+				while "\"" not in words[0]:
 					temp_list_for_title.append(words.pop(0))
-				location_title = ('_').join(temp_list_for_title)
+				location_title = ("_").join(temp_list_for_title)
 				location_id = self.location_title_to_id_map[location_title]
 
 				# Build the term vocabulary and also the dictionary for terms corresponding to the locations and their scores
@@ -62,16 +62,16 @@ class Task6:
 						if not self.location_dictionary.get(location_id):
 							self.location_dictionary[location_id] = {}
 						if not self.location_dictionary.get(location_id).get(current_word):
-							self.location_dictionary[location_id][current_word] = { 'TF': 0, 'DF': 0, 'TFIDF': 0}
+							self.location_dictionary[location_id][current_word] = { "TF": 0, "DF": 0, "TFIDF": 0}
 					elif index_mod4 == 1: # TF
-						self.location_dictionary[location_id][current_word]['TF'] = int(word)
+						self.location_dictionary[location_id][current_word]["TF"] = int(word)
 					elif index_mod4 == 2: # DF
-						self.location_dictionary[location_id][current_word]['DF'] = int(word)
+						self.location_dictionary[location_id][current_word]["DF"] = int(word)
 					elif index_mod4 == 3: # TFIDF
-						self.location_dictionary[location_id][current_word]['TFIDF'] = float(word)
+						self.location_dictionary[location_id][current_word]["TFIDF"] = float(word)
 
 	def construct_similarity_matrix(self, model):
-		'''
+		"""
 		Method Explanation:
 			. Goes over every location as a potential query location, compares its textual descriptors with every other location as a
 			  potential target location.
@@ -79,8 +79,8 @@ class Task6:
 		Inputs:
 			<model> - Has three possible values -- TF, DF, TFIDF. Corresponds to which model score to consider for computing the Cosine Similarity
 			          between the textual descriptors.
-		'''
-		the_model = 'TFIDF'
+		"""
+		the_model = model
 		# Go over every location as a potential query location
 		for query_location_id in self.location_list:
 			query_model_vector = [0] * self.global_term_dictionary_current_index
@@ -114,18 +114,18 @@ class Task6:
 					self.similarity_matrix[query_location_id - 1][target_location_id - 1] = cosine_similarity_value
 	
 	def print_k_latent_semantics(self, k):
-		'''
+		"""
 		Method Explanation:
 			. Applies a Singular Valued Decomposition on the similarity matrix and prints the first k latent semantics determined by the k parameter.
 			. The output is in the form of location-weight pairs for each semantic sorted in the decreasing order of weights.
 		Input:
 			. <k> for considering only the k latent semantics post SVD
-		'''
+		"""
 		U, S, Vt = numpy.linalg.svd(self.similarity_matrix)
 
 		# {
-		#  <location_id>: [{'Location Name': <>, 'Weight': <>}, {'Location Name': <>, 'Weight': <>}, ...],
-		#  <location_id>: [{'Location Name': <>, 'Weight': <>}, {'Location Name': <>, 'Weight': <>}, ...],
+		#  <location_id>: [{"Location Name": <>, "Weight": <>}, {"Location Name": <>, "Weight": <>}, ...],
+		#  <location_id>: [{"Location Name": <>, "Weight": <>}, {"Location Name": <>, "Weight": <>}, ...],
 		#  ...
 		# }
 		semantic_data_dict = {}
@@ -134,22 +134,22 @@ class Task6:
 				semantic_data_dict[arr_index+1] = []
 
 			for index, element in enumerate(arr):
-				semantic_data_dict[arr_index+1].append({ 'Location Name': self.location_id_to_title_map[str(index+1)], 'Weight': element })
+				semantic_data_dict[arr_index+1].append({ "Location Name": self.location_id_to_title_map[str(index+1)], "Weight": element })
 
 			# Sort the list based on the weight attribute
-			sorted_list = sorted(semantic_data_dict[arr_index+1], key=itemgetter('Weight'), reverse=True)
+			sorted_list = sorted(semantic_data_dict[arr_index+1], key=itemgetter("Weight"), reverse=True)
 			semantic_data_dict[arr_index+1].clear()
 			semantic_data_dict[arr_index+1] = sorted_list
 
 			# Print the latent semantic as location name-weight pairs sorted in decreasing order of weights
-			print('Latent Semantic: ', arr_index+1)
+			print("Latent Semantic: ", arr_index+1)
 			for idx, data in enumerate(sorted_list):
-				print('\tLocation Name: ', semantic_data_dict[arr_index+1][idx]['Location Name'], '| Weight: ', semantic_data_dict[arr_index+1][idx]['Weight'])
+				print("\tLocation Name: ", semantic_data_dict[arr_index+1][idx]["Location Name"], "| Weight: ", semantic_data_dict[arr_index+1][idx]["Weight"])
 
 	def runner(self):
-		k = input('Enter the k value: ')
+		k = input("Enter the k value: ")
 		k = int(k)
-
+		the_model = "TFIDF"
 		self.construct_vocabulary()
-		self.construct_similarity_matrix('TFIDF')
+		self.construct_similarity_matrix(the_model)
 		self.print_k_latent_semantics(k)
